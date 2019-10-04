3c97edd02d1f61e7f8dc10233fc4675145d5432e
Peter <phefland@gmail.com>
2019-10-03 23:50:26 -0300

    Adding and consuming Context for Todos.

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import useToggleState from "./hooks/useToggleState";
 import EditTodoForm from "./EditTodoForm";
 import ListItem from "@material-ui/core/ListItem";
@@ -8,25 +8,15 @@ import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import { TodosContext } from "./context/todos.context";
 
-export default function Todo({
-  task,
-  completed,
-  removeTodo,
-  id,
-  toggleTodo,
-  editTodo
-}) {
+export default function Todo({ task, completed, id }) {
+  const { removeTodo, toggleTodo } = useContext(TodosContext);
   const [isEditing, toggle] = useToggleState(false);
   return (
     <ListItem style={{ height: "64px" }}>
       {isEditing ? (
-        <EditTodoForm
-          editTodo={editTodo}
-          id={id}
-          task={task}
-          toggleEditForm={toggle}
-        />
+        <EditTodoForm id={id} task={task} toggleEditForm={toggle} />
       ) : (
         <>
           <Checkbox
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import useTodoState from "./hooks/useTodoState";
+import { TodosProvider } from "./context/todos.context";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 
@@ -10,11 +10,6 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 
 function TodoApp() {
-  const initialTodos = [{ id: 1, task: "Pet a Monkey", completed: false }];
-  const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(
-    initialTodos
-  );
-
   return (
     <Paper
       style={{
@@ -32,13 +27,10 @@ function TodoApp() {
       </AppBar>
       <Grid container justify='center' style={{ marginTop: "1rem" }}>
         <Grid item xs={11} md={8} lg={4}>
-          <TodoForm addTodo={addTodo} />
-          <TodoList
-            todos={todos}
-            removeTodo={removeTodo}
-            toggleTodo={toggleTodo}
-            editTodo={editTodo}
-          />
+          <TodosProvider>
+            <TodoForm />
+            <TodoList />
+          </TodosProvider>
         </Grid>
       </Grid>
     </Paper>
